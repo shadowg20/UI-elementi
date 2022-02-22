fb0516007d4f8526ed69f13d0f2a34b5f0fe332a
Pauls <gustavspauls1@gmail.com>
2022-02-22 15:31:46 +0200

    Binu var kustinat kamer iet programma

---
+++ Assets/Skripti/Audioarskanu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Audioarskanu : MonoBehaviour {
+	public AudioSource mrBean;
+	public AudioClip themeSong;
+	public void hoverToBeanImage(){
+		mrBean.PlayOneShot (themeSong);
+	}
+	public void NOThoverToBeanImage(){
+		mrBean.Stop();
+	}
+}
---
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Lietos interfeisus, lai nodrosinatu peles funkcijas
+using UnityEngine.EventSystems;
+
+
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	private RectTransform transformacijuLogs;
+	public Canvas kanva;
+
+	void Start () {
+		transformacijuLogs = GetComponent<RectTransform> ();
+	}
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais klikskis uz objekta");
+	}
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta objekta vilksana");
+	}
+	public void OnDrag(PointerEventData notikums){
+		transformacijuLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Objekta vilksana partraukta!");
+	}
+
+}
